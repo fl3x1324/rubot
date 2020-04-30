d0d4898f68d651b1a271543af2bf5aedc9a37b2e
Atanas Minkov <atanas.minkov@icloud.com>
2020-04-30 05:27:44 +0300

    try and post back a reply

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,6 @@
 class WebhookController < ApplicationController
+  require 'net/http'
+
   def initialize
     super
     @json = ActiveSupport::JSON
@@ -10,7 +12,25 @@ class WebhookController < ApplicationController
       puts "JSON: #{request.body.read}"
       messages = event["entry"][0]["messaging"]
       messages.each do |msg|
-        puts "Got new message: #{msg["message"]["text"]} from sender id: #{msg["sender"]["id"]}"
+        puts "Got new message: #{msg.dig("message", "text")} from sender id: #{msg.dig("sender", "id")}"
+        reply = {
+            messaging_type: "RESPONSE",
+            recipient: {
+                id: msg.dig("sender", "id"),
+            },
+            message: {
+                text: "Слава Богу!",
+            },
+        }
+        puts "Replying with message: #{reply.to_json}"
+        uri = URI ["SEND_API_URL"]
+        req = Net::HTTP::Post.new uri
+        req["Content-Type"] = "application/json"
+        req.body = reply.to_json
+        res = Net::HTTP.start(uri.hostname, uri.port) { |http|
+          http.request req
+        }
+        puts res.to_json
       end
       hist_rec = HistoryRecord.new request_dump: request.body.read
       puts "History record persisted? #{hist_rec.save}"
